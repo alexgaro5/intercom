0aa295449750bf4eabbf4228cc5ae7fa508a3a5c
byMagg <byMag@hotmail.es>
2019-10-13 16:52:49 +0200

    comentarios

--- intercom_buffer_endian.py
+++ intercom_buffer_endian.py
@@ -2,15 +2,16 @@ import sounddevice as sd                                                        
 import numpy                                                                    # https://numpy.org/
 import socket                                                                   # https://docs.python.org/3/library/socket.html
 import sys
-from intercom import Intercom
 import struct
+from intercom import Intercom                                                   #Importing the original Intercom
 
 class IntercomBuffer(Intercom):
 
+    #Redifining the init method
     def init(self, args):
         Intercom.init(self, args)
-        self.chunk_to_play = 0
-        self.delay = self.buffer_capacity // 2
+        self.chunk_to_play = 0                                                  #This is the chunk we are going to play
+        self.delay = self.buffer_capacity // 2                                  #This is the delay we are using before playing the chunks
 
     def run(self):
         sending_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -18,26 +19,26 @@ class IntercomBuffer(Intercom):
         listening_endpoint = ("0.0.0.0", self.listening_port)
         receiving_sock.bind(listening_endpoint)
 
-        lista = [numpy.zeros((self.samples_per_chunk, self.number_of_channels), self.dtype)]*self.buffer_capacity
+        list = [numpy.zeros((self.samples_per_chunk, self.number_of_channels), self.dtype)]*self.buffer_capacity    #Defining the structure of the buffer filled with 0
 
         def receive_and_buffer():
-            message, source_address = receiving_sock.recvfrom(self.max_packet_size)
+            message, source_address = receiving_sock.recvfrom(self.max_packet_size)                                 #We recieve the message via UDP
 
-            pos, *array = struct.unpack('<H{}h'.format(self.samples_per_chunk * self.number_of_channels), message)
+            pos, *array = struct.unpack('<H{}h'.format(self.samples_per_chunk * self.number_of_channels), message)  #Unpacking the message recieved 
 
-            lista[(pos + self.delay) % self.buffer_capacity] = array
+            list[(pos + self.delay) % self.buffer_capacity] = array                                                 #Inserting the data audio in the buffer with the delay
         
         def record_send_and_play (indata, outdata, frames, time, status):
-            array = numpy.frombuffer(indata, dtype=self.dtype)
+            array = numpy.frombuffer(indata, dtype=self.dtype)                                                      #Inserting indata into numpy array with the specified type
             
-            array = struct.pack('<H{}h'.format(self.samples_per_chunk * self.number_of_channels), self.chunk_to_play,  *array)
+            array = struct.pack('<H{}h'.format(self.samples_per_chunk * self.number_of_channels), self.chunk_to_play,  *array)  #Packing the message to send
             
-            message = lista[self.chunk_to_play]                                          
-            self.chunk_to_play = (self.chunk_to_play + 1) % self.buffer_capacity
+            message = list[self.chunk_to_play]                                                                      #Getting the message from the buffer                                     
+            self.chunk_to_play = (self.chunk_to_play + 1) % self.buffer_capacity                                    #Incrementing the chunk_to_play
 
             sending_sock.sendto(array, (self.destination_IP_addr, self.destination_port))
 
-            outdata[:] = numpy.reshape(message, (self.samples_per_chunk, self.number_of_channels))
+            outdata[:] = numpy.reshape(message, (self.samples_per_chunk, self.number_of_channels))                  #Playing the audio recieved
 
             sys.stderr.write("."); sys.stderr.flush()
 
