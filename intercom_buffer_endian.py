2ae41d2f77dab7a5158f1af255160841aacd74f1
Manuel Marin Peral <mpmanuel98@gmail.com>
2019-10-14 17:04:33 +0200

    update

--- intercom_buffer_endian.py
+++ intercom_buffer_endian.py
@@ -19,21 +19,21 @@ class IntercomBuffer(Intercom):
         listening_endpoint = ("0.0.0.0", self.listening_port)
         receiving_sock.bind(listening_endpoint)
 
-        list = [numpy.zeros((self.samples_per_chunk, self.number_of_channels), self.dtype)]*self.buffer_capacity    #Defining the structure of the buffer filled with 0
+        lista = [numpy.zeros((self.samples_per_chunk, self.number_of_channels), self.dtype)]*self.buffer_capacity    #Defining the structure of the buffer filled with 0
 
         def receive_and_buffer():
             package, source_address = receiving_sock.recvfrom(self.max_packet_size)                                 #We recieve the message via UDP
 
             pos, *message = struct.unpack('<H{}h'.format(self.samples_per_chunk * self.number_of_channels), package)  #Unpacking the message recieved 
 
-            list[(pos + self.delay) % self.buffer_capacity] = message                                                 #Inserting the data audio in the buffer with the delay
+            lista[(pos + self.delay) % self.buffer_capacity] = message                                                 #Inserting the data audio in the buffer with the delay
         
         def record_send_and_play (indata, outdata, frames, time, status):
             array = numpy.frombuffer(indata, dtype=self.dtype)                                                      #Inserting indata into numpy array with the specified type
             
             package = struct.pack('<H{}h'.format(self.samples_per_chunk * self.number_of_channels), self.chunk_to_play, *array)  #Packing the message to send
             
-            message = list[self.chunk_to_play]                                                                      #Getting the message from the buffer                                     
+            message = lista[self.chunk_to_play]                                                                      #Getting the message from the buffer                                     
             self.chunk_to_play = (self.chunk_to_play + 1) % self.buffer_capacity                                    #Incrementing the chunk_to_play
 
             sending_sock.sendto(package, (self.destination_IP_addr, self.destination_port))
