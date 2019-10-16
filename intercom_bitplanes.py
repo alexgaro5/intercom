66d7d636d322edb6c253d0e844ae5e77f22f69c5
Alejandro Garcia <RGR361@inlumine.ual.es>
2019-10-16 17:00:59 +0200

    update

--- intercom_bitplanes.py
+++ intercom_bitplanes.py
@@ -1,9 +1,8 @@
-# Adding a buffer.
-
 import sounddevice as sd
 import numpy as np
 import struct
 from intercom_buffer import Intercom_buffer
+from intercom import Intercom
 
 if __debug__:
     import sys
@@ -19,7 +18,9 @@ class Intercom_bitplanes(Intercom_buffer):
 
         def receive_and_buffer():
             message, source_address = self.receiving_sock.recvfrom(Intercom.MAX_MESSAGE_SIZE)
+            ###################
 
+            ###################
             chunk_number, *chunk = struct.unpack(self.packet_format, message)
 
             self._buffer[chunk_number % self.cells_in_buffer] = np.asarray(chunk).reshape(self.frames_per_chunk, self.number_of_channels)
@@ -27,6 +28,9 @@ class Intercom_bitplanes(Intercom_buffer):
             return chunk_number
 
         def record_send_and_play(indata, outdata, frames, time, status):
+            ###################
+            print(indata)
+            ###################
             message = struct.pack(self.packet_format, self.recorded_chunk_number, *(indata.flatten()))
             self.recorded_chunk_number = (self.recorded_chunk_number + 1) % self.MAX_CHUNK_NUMBER
 
@@ -38,8 +42,8 @@ class Intercom_bitplanes(Intercom_buffer):
 
             outdata[:] = chunk
 
-            if __debug__:
-                sys.stderr.write("."); sys.stderr.flush()
+            #if __debug__:
+            #    sys.stderr.write("."); sys.stderr.flush()
 
         with sd.Stream(samplerate=self.frames_per_second, 
                        blocksize=self.frames_per_chunk, 
@@ -51,11 +55,6 @@ class Intercom_bitplanes(Intercom_buffer):
             self.played_chunk_number = (first_received_chunk_number - self.chunks_to_buffer) % self.cells_in_buffer
             while True:
                 receive_and_buffer()
-
-    def add_args(self):
-        parser = Intercom.add_args(self)
-        parser.add_argument("-cb", "--chunks_to_buffer", help="Number of chunks to buffer", type=int, default=32)
-        return parser
 
 if __name__ == "__main__":
     intercom = Intercom_bitplanes()
