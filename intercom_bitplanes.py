d00499aed8cde6188919ac643b55527bb699733e
Alejandro Garcia <RGR361@inlumine.ual.es>
2019-10-22 09:38:20 +0200

    update

--- intercom_bitplanes.py
+++ intercom_bitplanes.py
@@ -21,18 +21,12 @@ class Intercom_bitplanes(Intercom_buffer):
 
             #print("BUFFER:", self._buffer)
             for i in range(15,-1,-1):
-                package, source_address = self.receiving_sock.recvfrom(Intercom.MAX_MESSAGE_SIZE)
-                chunk_number, self.number_of_bitplane, *bitplane = struct.unpack(self.packet_format, package)
-                bitplane = np.asarray(bitplane)
-
-                print(bitplane << self.number_of_bitplane)
-                
-                self._buffer[chunk_number % self.cells_in_buffer][:,0] |= (bitplane << self.number_of_bitplane)
-
-                package, source_address = self.receiving_sock.recvfrom(Intercom.MAX_MESSAGE_SIZE)
-                chunk_number, self.number_of_bitplane, *bitplane = struct.unpack(self.packet_format, package)
-                bitplane = np.asarray(bitplane)
-                self._buffer[chunk_number % self.cells_in_buffer][:,1] |= (bitplane << self.number_of_bitplane)
+                for j in range (0, self.number_of_channels):
+                    package, source_address = self.receiving_sock.recvfrom(Intercom.MAX_MESSAGE_SIZE)
+                    chunk_number, self.number_of_bitplane, *bitplane = struct.unpack(self.packet_format, package)
+                    bitplane = np.asarray(bitplane)
+                    #print(bitplane << self.number_of_bitplane)
+                    self._buffer[chunk_number % self.cells_in_buffer][:,j] |= (bitplane << self.number_of_bitplane)
             
             #print("CHUNK_NUMBER",chunk_number,"INDATA RECEIVED", self._buffer[chunk_number % self.cells_in_buffer])            
             return chunk_number
@@ -43,14 +37,10 @@ class Intercom_bitplanes(Intercom_buffer):
 
             for i in range(15,-1,-1):
                 array = (indata & (1 << i)) >> i
-                canal1 = array[:,0]
-                canal2 = array[:,1]
 
-                message = struct.pack(self.packet_format, self.recorded_chunk_number, i, *canal1)
-                self.sending_sock.sendto(message, (self.destination_IP_addr, self.destination_port))
-
-                message = struct.pack(self.packet_format, self.recorded_chunk_number, i, *canal2)
-                self.sending_sock.sendto(message, (self.destination_IP_addr, self.destination_port))
+                for j in range (0, self.number_of_channels):
+                    message = struct.pack(self.packet_format, self.recorded_chunk_number, i, *array[:,j])
+                    self.sending_sock.sendto(message, (self.destination_IP_addr, self.destination_port))
 
             self.recorded_chunk_number = (self.recorded_chunk_number + 1) % self.MAX_CHUNK_NUMBER
             chunk = self._buffer[self.played_chunk_number % self.cells_in_buffer]
@@ -59,8 +49,8 @@ class Intercom_bitplanes(Intercom_buffer):
 
             outdata[:] = chunk
 
-            #if __debug__:
-            #    sys.stderr.write("."); sys.stderr.flush()
+            if __debug__:
+                sys.stderr.write("."); sys.stderr.flush()
 
         with sd.Stream(samplerate=self.frames_per_second, 
                        blocksize=self.frames_per_chunk, 
