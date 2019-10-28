e194872909de1ca0c0dee9ba2848fd7075e1aa81
Daniel Barroso <dbu104@inlumine.ual.es>
2019-10-28 23:12:13 +0100

    update

--- intercom_bitplanes_nuevo.py
+++ intercom_bitplanes_nuevo.py
@@ -12,28 +12,19 @@ class Intercom_bitplanes_nuevo(Intercom_bitplanes):
 
     def init(self, args):
         Intercom_bitplanes.init(self, args)
-        #Formado Recorded_chunk, significativo, channel y bitplane
-        self.packet_format = f"!HBB{self.frames_per_chunk//8}B"
+        self.packet_format = f"!HBB{self.frames_per_chunk//8}B"     #Formado Recorded_chunk, significativo, channel y bitplane
 
     def record_send_and_play_two_channel(self, indata, outdata, frames, time, status):
-
+        
         indata[:,0] -= indata[:,1]
-
         #Recorremos el indata y vamos cogiendo columnas de mas a menos significativo.
         for significant in range(15,-1,-1):
-            #De las columnas, cogemos un canal.
-            for channel in range (0, self.number_of_channels):
-                #Cogemos la columna.
-                array = (indata & (1 << significant)) >> significant
-                #Cogemos el canal.
-                array_channel = array[:,channel]
-                #Lo pasamos a int8.
-                channel_int8 = array_channel.astype(np.uint8)
-                #Lo compactamos
-                channelpackbits = np.packbits(channel_int8)
-                #Lo empaquetamos en el formado especificado anteriormente.
-                message = struct.pack(self.packet_format, self.recorded_chunk_number, significant, channel, *channelpackbits)
-                #Enviamos el paquete.
+            for channel in range (0, self.number_of_channels):  #De las columnas, cogemos un canal.
+                array = (indata & (1 << significant)) >> significant    #Cogemos la columna.
+                array_channel = array[:,channel]                        #Lo pasamos a int8.
+                channel_int8 = array_channel.astype(np.uint8)           #Lo compactamos
+                channelpackbits = np.packbits(channel_int8)             #Lo empaquetamos en el formado especificado anteriormente.
+                message = struct.pack(self.packet_format, self.recorded_chunk_number, significant, channel, *channelpackbits)   #Enviamos el paquete.
                 self.sending_sock.sendto(message, (self.destination_IP_addr, self.destination_port))
 
         self.recorded_chunk_number = (self.recorded_chunk_number + 1) % self.MAX_CHUNK_NUMBER
