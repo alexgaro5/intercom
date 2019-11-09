3669229fc094fdef4b231e67f34e9a87d427b4d3
Alejandro Garcia <RGR361@inlumine.ual.es>
2019-11-09 10:56:35 +0100

    update

--- intercom_binaural.py
+++ intercom_binaural.py
@@ -1,13 +1,6 @@
 # Exploiting binaural redundancy.
-import sounddevice as sd
-import numpy as np
-import struct
-from intercom import Intercom
-from intercom_buffer import Intercom_buffer
-from intercom_bitplanes import Intercom_bitplanes
 
-if __debug__:
-    import sys
+from intercom_bitplanes import Intercom_bitplanes
 
 class Intercom_binaural(Intercom_bitplanes):
 
