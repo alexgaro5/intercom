7a5829c8c30b61a4cf46fbe718c3df2baca615e5
alexgaro5 <alexgarohotmailcom@gmail.com>
2019-11-12 12:36:43 +0100

    update

--- EjecutadorGeneral.bat
+++ EjecutadorGeneral.bat
@@ -1,5 +1,6 @@
 @echo off
 
 set /p nombre=Archivo a ejecutar, sin "intercom_" ni ".py", solo el nombre: 
-set /p canal=¿Uno o dos canales?: 
+set /p canal=Uno o dos canales: 
 python intercom_%nombre%.py -c %canal%
+PAUSE
